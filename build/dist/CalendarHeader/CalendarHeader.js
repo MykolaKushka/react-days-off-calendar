30200582cf0d30350acb56bcecbd78f742f6d9cc
Mykola Kushka <Mykola.Kushka@gmail.com>
2021-10-21 18:44:34 +0300

    Delete build

--- build/dist/CalendarHeader/CalendarHeader.js
+++
@@ -1,6 +0,0 @@
-import React from "../../_snowpack/pkg/react.js";
-export const CalendarHeader = () => {
-  return /* @__PURE__ */ React.createElement("div", {
-    className: "mainheader"
-  }, /* @__PURE__ */ React.createElement("h1", null, "Days Off Calendar"));
-};
--- build/dist/CheckboxFilters/CheckboxFilters.js
+++
@@ -1,7 +0,0 @@
-import React from "../../_snowpack/pkg/react.js";
-export const CheckboxFilters = () => {
-  return /* @__PURE__ */ React.createElement("div", {
-    id: "calendarFilter",
-    className: "calendars-checkbox"
-  });
-};
